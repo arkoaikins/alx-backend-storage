f6572ed5d03e0bbc6f5ae6305d15df49776f09bf
arkoaikins <arkoaikins@gmail.com>
2024-01-17 18:56:11 -0500

    Write a SQL script that creates a stored procedure

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,8 +5,11 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-  DECLARE avg_score INT
-  SELECT AVG(score) INTO avg_score FROM corrections WHERE corrections.user_id = user_id
-  UPDATE users SET average_score = avg_score WHERE id = user_id
-END //
+UPDATE users
+SET average_score = (
+    SELECT AVG(score) FROM corrections
+    WHERE corrections.user_id = user_id
+)
+WHERE id = user_id;
+END $$
 DELIMITER ;
