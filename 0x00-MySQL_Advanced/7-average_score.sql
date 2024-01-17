4d91b98d4c0abecb59fa9c513f2ff0639e4708cb
arkoaikins <arkoaikins@gmail.com>
2024-01-17 18:37:55 -0500

    Write a SQL script that creates a stored procedure

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,6 +1,6 @@
 -- Write a SQL script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student.
--- Note: An average score can be a decimal
+-- An average score can be a decimal
 
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
