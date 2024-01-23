9a50490d45d94b220ee29493baf3d08b6d664523
arkoaikins <arkoaikins@gmail.com>
2024-01-23 06:50:36 -0800

    [task8]:Write a Python function that lists all documents in a collection:

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Python function that lists all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """ Lists all doc in collection"""
+    return mongo_collection.find()
