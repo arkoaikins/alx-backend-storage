28464ce61cdc08f056d7b974d35ba227da629e85
arkoaikins <arkoaikins@gmail.com>
2024-01-24 12:47:33 -0500

    [task1]:add get method to get original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,12 +4,13 @@ Writing strings to Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
     """stores an instance of the Redis client"""
     def __init__(self):
+        """Initializing a cache instance"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
@@ -18,3 +19,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str,
+                                                          bytes, int, float]:
+        """retrieve original data type from storage"""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """parametize Cache.get with correct conversion func"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """parametize Cache.get with correct conversion func"""
+        return self.get(key, fn=int)
